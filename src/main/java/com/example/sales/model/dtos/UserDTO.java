0ba7110320e77daeebef0615800aaff6a99d51a7
JoseVictorMarques <josevictor.marques1998@gmail.com>
2024-04-17 22:31:26 -0300

    change password endpoint

---
+++ src/main/java/com/example/sales/model/dtos/UserDTO.java
@@ -0,0 +1,13 @@
+package com.example.sales.model.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+    private Long id;
+    private String username;
+    private String password;
+    private String newPassword;
+}
