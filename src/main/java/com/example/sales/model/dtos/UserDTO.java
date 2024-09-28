b6ba6c98f5e2be03197b109d048e5c942fccfc58
JoseVictorMarques <josevictor.marques1998@gmail.com>
2024-09-27 23:59:44 -0300

    adding junit dependencies and creating user tests

--- src/main/java/com/example/sales/model/dtos/UserDTO.java
+++ src/main/java/com/example/sales/model/dtos/UserDTO.java
@@ -1,10 +1,14 @@
 package com.example.sales.model.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
     private Long id;
     private String username;
--- src/main/java/com/example/sales/tests/UserTest.java
+++ src/main/java/com/example/sales/tests/UserTest.java
@@ -1,2 +1,71 @@
-package com.example.sales.tests;public class UserTest {
+package com.example.sales.tests;
+
+import com.example.sales.business.UserBusiness;
+import com.example.sales.model.dtos.UserDTO;
+import com.example.sales.model.entities.User;
+import com.example.sales.repository.UserRepository;
+import com.example.sales.utils.PasswordUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+public class UserTest {
+
+    @InjectMocks
+    private UserBusiness userBusiness;
+    @Mock
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testRegisterUser() {
+        User user = new User("john", "password123");
+        User savedUser = new User("john", PasswordUtils.hashPassword("password123"));
+
+        when(userRepository.save(any(User.class))).thenReturn(savedUser);
+
+        User result = userBusiness.registerUser(user);
+
+        assertNotNull(result);
+        assertEquals(savedUser.getUsername(), result.getUsername());
+        assertNotEquals("password123", result.getPassword());
+    }
+
+    @Test
+    public void testLoginSuccess() {
+        User user = new User("john", "password123");
+        User userDB = new User("john", PasswordUtils.hashPassword("password123"));
+
+        when(userRepository.findUserByUsername(user.getUsername())).thenReturn(userDB);
+        when(PasswordUtils.verifyPassword(user.getPassword(), userDB.getPassword())).thenReturn(true);
+
+        Boolean loginResult = userBusiness.login(user);
+
+        assertTrue(loginResult);
+    }
+
+    @Test
+    public void testChangePasswordSuccess() {
+        UserDTO userDTO = new UserDTO(null, "john", null,"newPassword");
+        User user = new User("john", PasswordUtils.hashPassword("oldPassword"));
+
+        when(userRepository.findUserByUsername(userDTO.getUsername())).thenReturn(user);
+        when(userRepository.save(any(User.class))).thenReturn(user);
+
+        User result = userBusiness.changePassword(userDTO);
+
+        assertNotNull(result);
+        assertEquals(userDTO.getUsername(), result.getUsername());
+        assertNotEquals("oldPassword", result.getPassword());
+    }
 }
